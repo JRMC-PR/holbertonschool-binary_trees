fbc4c565fd175ead06546dd7d3cec01f6ed87b19
GuilleP2018 <gpereyo@gmail.com>
2023-09-12 18:27:07 +0000

    completed 0-binary_tree_node

--- binary_trees.h
+++ binary_trees.h
@@ -14,15 +14,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+}binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 /** additional binary tree options **/
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -47,5 +46,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 
 #endif
