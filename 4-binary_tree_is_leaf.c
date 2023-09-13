0285d3a2ba7a5ae00864a8dc1ca243f151738f1f
GuilleP2018 <gpereyo@gmail.com>
2023-09-13 18:05:09 +0000

    betty fixes for is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_lead - function to check if node is a leaf
+ * binary_tree_is_leaf - function to check if node is a leaf
  * @node: pointer to the node you are checking
  * Return: 1 if its a leaf, 0 if not
  */
